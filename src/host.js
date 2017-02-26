4aad94c25889bc58deee75fd1ba430110bd9b9a6
Casey Curry <casey.de.curry@gmail.com>
2017-02-26 11:52:22 -0600

    fix client initializer arguments

--- src/host.js
+++ src/host.js
@@ -6,4 +6,4 @@ const path = require("path");
 const express = require("express");
 
 const staticFileLocation = path.join(__dirname, "app");
-clientInitializer.initialize(express, "users-client", staticFileLocation);
+clientInitializer.initialize("users-client", staticFileLocation);
