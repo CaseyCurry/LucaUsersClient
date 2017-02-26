6d43431d7911541b345b6832353ebc30ee8745bf
Casey Curry <casey.de.curry@gmail.com>
2017-02-26 11:58:26 -0600

    remove orphaned express variable

--- src/host.js
+++ src/host.js
@@ -3,7 +3,6 @@
 
 const clientInitializer = require("luca-client-initializer");
 const path = require("path");
-const express = require("express");
 
 const staticFileLocation = path.join(__dirname, "app");
 clientInitializer.initialize("users-client", staticFileLocation);
