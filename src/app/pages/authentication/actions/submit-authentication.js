d8edc00581fe63081088eea8c55b6fe695300a51
CaseyCurry <casey.de.curry@gmail.com>
2017-09-10 11:15:54 -0500

    revert "unpromisify client of api.getUrl()"
    This reverts commit 4ed3b46c21df63e62e144eb77b1cacee421dfdfc.

--- src/app/pages/authentication/actions/submit-authentication.js
+++ src/app/pages/authentication/actions/submit-authentication.js
@@ -1,16 +1,21 @@
 const action = (api) => {
   return (email, password) => {
-    const url = api.getUrl();
     return {
       type: "SUBMIT_AUTHENTICATION",
-      payload: api.client.post(`${url}/${encodeURIComponent(email)}`, {
-        "password": password
-      }, {
-        validateStatus: (status) => {
-          console.log(status);
-          return status == 200 || status == 401 || status == 404;
-        }
-      })
+      payload: api.getUrl()
+        .then(url => {
+          return api.client.post(`${url}/${encodeURIComponent(email)}`, {
+            "password": password
+          }, {
+            validateStatus: (status) => {
+              console.log(status);
+              return status == 200 || status == 401 || status == 404;
+            }
+          });
+        })
+        .catch(error => {
+          Promise.reject(error);
+        })
     };
   };
 };
--- src/app/pages/registration/actions/submit-registration.js
+++ src/app/pages/registration/actions/submit-registration.js
@@ -1,17 +1,22 @@
 const action = (api) => {
   return (email, password) => {
-    const url = api.getUrl();
     return {
       type: "SUBMIT_REGISTRATION",
-      payload: api.client.post(url, {
-        "email": email,
-        "password": password
-      }, {
-        validateStatus: (status) => {
-          console.log(status);
-          return status == 200 || status == 409;
-        }
-      })
+      payload: api.getUrl()
+        .then(url => {
+          return api.client.post(url, {
+            "email": email,
+            "password": password
+          }, {
+            validateStatus: (status) => {
+              console.log(status);
+              return status == 200 || status == 409;
+            }
+          });
+        })
+        .catch(error => {
+          Promise.reject(error);
+        })
     };
   };
 };
