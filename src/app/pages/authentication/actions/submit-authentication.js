4ed3b46c21df63e62e144eb77b1cacee421dfdfc
CaseyCurry <casey.de.curry@gmail.com>
2017-09-10 10:50:23 -0500

    unpromisify client of api.getUrl()

--- src/app/pages/authentication/actions/submit-authentication.js
+++ src/app/pages/authentication/actions/submit-authentication.js
@@ -1,21 +1,16 @@
 const action = (api) => {
   return (email, password) => {
+    const url = api.getUrl();
     return {
       type: "SUBMIT_AUTHENTICATION",
-      payload: api.getUrl()
-        .then(url => {
-          return api.client.post(`${url}/${encodeURIComponent(email)}`, {
-            "password": password
-          }, {
-            validateStatus: (status) => {
-              console.log(status);
-              return status == 200 || status == 401 || status == 404;
-            }
-          });
-        })
-        .catch(error => {
-          Promise.reject(error);
-        })
+      payload: api.client.post(`${url}/${encodeURIComponent(email)}`, {
+        "password": password
+      }, {
+        validateStatus: (status) => {
+          console.log(status);
+          return status == 200 || status == 401 || status == 404;
+        }
+      })
     };
   };
 };
--- src/app/pages/registration/actions/submit-registration.js
+++ src/app/pages/registration/actions/submit-registration.js
@@ -1,22 +1,17 @@
 const action = (api) => {
   return (email, password) => {
+    const url = api.getUrl();
     return {
       type: "SUBMIT_REGISTRATION",
-      payload: api.getUrl()
-        .then(url => {
-          return api.client.post(url, {
-            "email": email,
-            "password": password
-          }, {
-            validateStatus: (status) => {
-              console.log(status);
-              return status == 200 || status == 409;
-            }
-          });
-        })
-        .catch(error => {
-          Promise.reject(error);
-        })
+      payload: api.client.post(url, {
+        "email": email,
+        "password": password
+      }, {
+        validateStatus: (status) => {
+          console.log(status);
+          return status == 200 || status == 409;
+        }
+      })
     };
   };
 };
